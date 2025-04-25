565cf10e6837eec5a7d90214195dff2e59e6609a
yashamte72 <yashamte123@gmail.com>
2025-04-25 23:55:27 +0530

    Update load_data.py

--- load_data.py
+++ load_data.py
@@ -4,10 +4,17 @@ import mysql.connector
 #load data from csv file
 df = pd.read.csv('data\AWCustomers.csv')
 
+try:
 #database connection 
-conn = mysql.connector.connect(
-    host = "localhost",
-    user = "root",
-    password = "1234",
-    database = "insightsmith"
-)
+    conn = mysql.connector.connect(
+        host = "localhost",
+        user = "root",
+        password = "1234",
+        database = "insightsmith"
+    )
+
+    if conn.is_connected():
+        print("connected to mysql database  successfully")
+    
+except:
+    print("error in connecting to mysql database")
