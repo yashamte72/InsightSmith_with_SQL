624bd684c8ced53d147092a12d7a82b78615bc63
yashamte72 <yashamte123@gmail.com>
2025-04-25 23:50:56 +0530

    Update load_data.py

--- load_data.py
+++ load_data.py
@@ -2,6 +2,12 @@ import pandas as pd
 import mysql.connector
 
 #load data from csv file
- df = pd.read.csv('data\AWCustomers.csv')
+df = pd.read.csv('data\AWCustomers.csv')
 
 #database connection 
+conn = mysql.connector.connect(
+    host = "localhost",
+    user = "root",
+    password = "1234",
+    database = "insightsmith"
+)
